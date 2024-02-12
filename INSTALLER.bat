6cb4467d1781cdc4e7927a6d712fe90676e8c102
Vo1dAbyss <153193919+Vo1dAbyss@users.noreply.github.com>
2024-02-12 23:19:35 +0100

    Update INSTALLER.bat

--- INSTALLER.bat
+++ INSTALLER.bat
@@ -1,10 +1,10 @@
 @echo off
-title DialoGPT-medium-GPT4
+title AIModel-Launcher-WebUI INSTALLER.bat
 color 0F
 echo ------------ DOWNLOADING GIT REPOSITORY ------------
 echo from time import sleep; sleep(1) | python
-git clone https://github.com/Vo1dAbyss/DialoGPT-medium-GPT4-Interface-Files.git
-cd DialoGPT-medium-GPT4-Interface-Files
+git clone https://github.com/Vo1dAbyss/AIModel-Launcher-WebUI-Files.git
+cd AIModel-Launcher-WebUI-Files
 echo ------------ INSTALLING REQUIREMENTS... ------------
 echo from time import sleep; sleep(1) | python
 pip install -r requirements.txt
